b70ce0b23a52fe1508e4dca7fdb320131a498222
Igor Galić <i.galic@brainsware.org>
2016-05-18 15:34:49 +0200

    add tests, reformat to puppet style

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,54 +1,44 @@
 # configures flannel
 class flannel::config {
 
-case $::osfamily {
-    'Debian': {
-      if ($::operatingsystem == 'ubuntu' and $::lsbdistcodename in ['lucid', 'precise', 'trusty'])
-      or ($::operatingsystem == 'debian' and $::operatingsystemmajrelease in ['6', '7', '8']) {
-          file { '/etc/flanneld':
-            ensure  => directory,
-            mode    => '0755',
-          }
-          file { '/etc/flanneld/flanneld.conf':
-            ensure  => file,
-            content => template("${module_name}/sysconfig/flanneld.erb"),
-            mode    => '0644',
-          }
-          file { '/etc/default/flanneld':
-            ensure  => file,
-            content => template("${module_name}/default/flanneld.erb"),
-            mode    => '0644',
-          }
-      }
+  if $::osfamily == 'Debian' {
+    file { '/etc/flanneld':
+      ensure => directory,
+      mode   => '0755',
     }
-    'RedHat': {
-      if ($::operatingsystem in ['RedHat', 'CentOS'] and $::operatingsystemmajrelease in ['5', '6', '7']) {
-          file { '/etc/sysconfig/flanneld':
-            ensure  => file,
-            content => template("${module_name}/sysconfig/flanneld.erb"),
-            mode    => '0644',
-          }
-
-          if $flannel::manage_docker {
-            $docker_dropin_ensure = 'file'
-            include ::docker
-            File['/usr/lib/systemd/system/docker.service.d/flannel.conf'] ~> Service['flanneld'] ~> Service['docker']
-          } else {
-            $docker_dropin_ensure = 'absent'
-          }
-
-          file { '/usr/lib/systemd/system/docker.service.d/flannel.conf':
-            ensure  => $docker_dropin_ensure,
-            content => template("${module_name}/service_flannel.conf"),
-            mode    => '0644',
-          } ~>
-          exec { 'reload systemctl daemon for flannel':
-            command     => '/bin/systemctl daemon-reload',
-            refreshonly => true,
-          }
-
-        }
-      }
+    file { '/etc/flanneld/flanneld.conf':
+      ensure  => file,
+      content => template("${module_name}/sysconfig/flanneld.erb"),
+      mode    => '0644',
+    }
+    file { '/etc/default/flanneld':
+      ensure  => file,
+      content => template("${module_name}/default/flanneld.erb"),
+      mode    => '0644',
+    }
+  } else { # 'RedHat'
+    file { '/etc/sysconfig/flanneld':
+      ensure  => file,
+      content => template("${module_name}/sysconfig/flanneld.erb"),
+      mode    => '0644',
     }
 
+    if $flannel::manage_docker {
+      $docker_dropin_ensure = 'file'
+      include ::docker
+      File['/usr/lib/systemd/system/docker.service.d/flannel.conf'] ~> Service['flanneld'] ~> Service['docker']
+    } else {
+      $docker_dropin_ensure = 'absent'
+    }
+
+    file { '/usr/lib/systemd/system/docker.service.d/flannel.conf':
+      ensure  => $docker_dropin_ensure,
+      content => template("${module_name}/service_flannel.conf"),
+      mode    => '0644',
+    } ~>
+    exec { 'reload systemctl daemon for flannel':
+      command     => '/bin/systemctl daemon-reload',
+      refreshonly => true,
+    }
+  }
 }
