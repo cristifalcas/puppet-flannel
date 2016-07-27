566ff081497a18c153460eaa6b4cd815accf7afd
Cristian Falcas <cristi.falcas@gmail.com>
2016-07-27 18:16:34 +0300

    make flannel work on el6 also

--- manifests/config.pp
+++ manifests/config.pp
@@ -24,39 +24,41 @@ class flannel::config {
       mode    => '0644',
     }
 
-    if $flannel::manage_docker {
-      $docker_dropin_ensure = 'file'
-      include ::docker
-      File['/usr/lib/systemd/system/docker.service.d/flannel.conf'] ~> Service['flanneld'] ~> Service['docker']
-    } else {
-      $docker_dropin_ensure = 'absent'
-    }
-
-    file { '/usr/lib/systemd/system/docker.service.d/flannel.conf':
-      ensure  => $docker_dropin_ensure,
-      content => template("${module_name}/service_flannel.conf"),
-      mode    => '0644',
-    } ~>
-    exec { 'reload systemctl daemon for flannel':
-      command     => '/bin/systemctl daemon-reload',
-      refreshonly => true,
-    }
-
-    if $flannel::journald_forward_enable and $::operatingsystemmajrelease == 7 {
-      file { '/etc/systemd/system/flanneld.service.d':
-        ensure => 'directory',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755',
+    if $::operatingsystemmajrelease == 7 {
+      if $flannel::manage_docker {
+        $docker_dropin_ensure = 'file'
+        include ::docker
+        File['/usr/lib/systemd/system/docker.service.d/flannel.conf'] ~> Service['flanneld'] ~> Service['docker']
+      } else {
+        $docker_dropin_ensure = 'absent'
       }
-      file { '/etc/systemd/system/flanneld.service.d/journald.conf':
-        ensure  => file,
-        owner   => 'root',
-        group   => 'root',
+
+      file { '/usr/lib/systemd/system/docker.service.d/flannel.conf':
+        ensure  => $docker_dropin_ensure,
+        content => template("${module_name}/service_flannel.conf"),
         mode    => '0644',
-        content => template("${module_name}/journald.conf.erb"),
       } ~>
-      Exec['reload systemctl daemon for flannel']
+      exec { 'reload systemctl daemon for flannel':
+        command     => '/bin/systemctl daemon-reload',
+        refreshonly => true,
+      }
+
+      if $flannel::journald_forward_enable {
+        file { '/etc/systemd/system/flanneld.service.d':
+          ensure => 'directory',
+          owner  => 'root',
+          group  => 'root',
+          mode   => '0755',
+        }
+        file { '/etc/systemd/system/flanneld.service.d/journald.conf':
+          ensure  => file,
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0644',
+          content => template("${module_name}/journald.conf.erb"),
+        } ~>
+        Exec['reload systemctl daemon for flannel']
+      }
     }
   } else {
     fail("Unsupported OS: ${::osfamily}")
